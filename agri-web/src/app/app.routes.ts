7d72c04bf40ae86012e913fe08b0e9fe46ca9429
robson_de_andrade <robsondeandrade23@gmail.com>
2025-06-29 15:33:00 -0400

    config routes for login

--- agri-web/src/app/app.routes.ts
+++ agri-web/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './domains/access-control/presentation/auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        title: 'Acesso',
+        component: LoginComponent
+    },
+    { 
+        path: '', 
+        redirectTo: 'login', 
+        pathMatch: 'full' 
+    },
+];
