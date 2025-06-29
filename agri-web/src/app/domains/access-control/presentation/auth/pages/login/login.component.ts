532fa8fdf11f9ba3f0231decb3919936cbb68dd5
robson_de_andrade <robsondeandrade23@gmail.com>
2025-06-29 15:31:23 -0400

    form group and form control

--- agri-web/src/app/domains/access-control/presentation/auth/pages/login/login.component.ts
+++ agri-web/src/app/domains/access-control/presentation/auth/pages/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  public h1: string = 'Insira suas credenciais para acessar sua conta';
+
+  loginForm = new FormGroup({
+    loginEmail: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]),
+    loginPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+  }
 
 }
