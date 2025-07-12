ae77eca0c41d1ea969c7ea3f65596e18f68879b7
robson_de_andrade <robsondeandrade23@gmail.com>
2025-07-12 13:06:22 -0400

    Created properties, form group, and added imports

--- agri-manager/src/app/domains/access-control/presentation/pages/login/login-page.component.ts
+++ agri-manager/src/app/domains/access-control/presentation/pages/login/login-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginComponent {
+
+  public h1: string = 'Insira suas credenciais para acessar sua conta';
+
+  loginForm = new FormGroup({
+    loginEmail: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]),
+    loginPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+  }
 
 }
