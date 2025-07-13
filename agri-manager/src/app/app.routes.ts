8c0c0502858e83d3bae326bf3154d134e6d04c50
robson_de_andrade <robsondeandrade23@gmail.com>
2025-07-13 10:35:05 -0400

    Created route for reset password component

--- agri-manager/src/app/app.routes.ts
+++ agri-manager/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './domains/access-control/presentation/pages/login/login-page.component';
 import { SignupComponent } from './domains/access-control/presentation/pages/signup/signup-page.component';
+import { ResetPasswordComponent } from './domains/access-control/presentation/pages/reset-password/reset-password-page.component';
 
 export const routes: Routes = [
     {
@@ -17,5 +18,10 @@ export const routes: Routes = [
         path: 'signup',
         title: 'Cadastro',
         component: SignupComponent
+    },
+    {
+        path: 'reset-password',
+        title: 'Redefinir Senha',
+        component: ResetPasswordComponent
     }
 ];
