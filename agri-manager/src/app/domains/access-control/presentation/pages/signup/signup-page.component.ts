10cf963c82aaf13cad5903b34edda6635df35895
robson_de_andrade <robsondeandrade23@gmail.com>
2025-07-13 10:22:10 -0400

    Created property and form group

--- agri-manager/src/app/domains/access-control/presentation/pages/signup/signup-page.component.ts
+++ agri-manager/src/app/domains/access-control/presentation/pages/signup/signup-page.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupComponent {
+
+  public h1: string = 'Preencha os campos para criar uma conta';
+
+  signupForm = new FormGroup({
+    name: new FormControl('', [Validators.minLength(6), Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.minLength(5), Validators.required]),
+    jobtitle: new FormControl('', [Validators.minLength(6), Validators.required]),
+  });
+
+  onSubmit() {
+    console.warn(this.signupForm.value);
+  }
 
 }
