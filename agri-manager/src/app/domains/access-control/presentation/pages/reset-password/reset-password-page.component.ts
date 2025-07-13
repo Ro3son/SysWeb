626cb6a8c9ba479d9ef0df965b1ba1c46385612c
robson_de_andrade <robsondeandrade23@gmail.com>
2025-07-13 10:51:02 -0400

    Added form group and router link

--- agri-manager/src/app/domains/access-control/presentation/pages/reset-password/reset-password-page.component.ts
+++ agri-manager/src/app/domains/access-control/presentation/pages/reset-password/reset-password-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './reset-password-page.component.html',
   styleUrl: './reset-password-page.component.css'
 })
 export class ResetPasswordComponent {
+
+  public h1: string = 'Redefinir senha';
+
+  resetPasswordForm = new FormGroup({
+    userEmail: new FormControl('', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]),
+  });
+
+  public onSubmit() {
+    window.localStorage.setItem('Senha redefinida', JSON.stringify(this.resetPasswordForm.value));
+    window.location.reload();
+  }
 
 }
